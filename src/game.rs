e789c2b4aad0098646325131944f31b1ffb5850b
ProgrammingIncluded <chencharlesh@gmail.com>
2022-02-09 23:39:03 -0800

    Yucky tree traversal POC

--- src/game.rs
+++ src/game.rs
@@ -1,26 +1,28 @@
 use crate::state::*;
 
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 use std::cell::RefCell;
 
 type Link<T> = Rc<RefCell<T>>;
+type WeakLink<T> = Weak<RefCell<T>>;
 
 #[derive(Debug)]
 pub struct Node {
     children: Vec<Link<Node>>,
-    parent: Option<Link<Node>>,
-    board: Box<Board>
+    parent: Option<WeakLink<Node>>,
+    board: Box<Board>,
 }
 
 impl Node {
     fn new(children: Vec<Link<Node>>,
-           parent: Option<Link<Node>>,
+           parent: Option<WeakLink<Node>>,
            board: Box<Board>) -> Link<Self> {
+
         return Rc::new(RefCell::new(
             Node {
                 children: children,
                 parent: parent,
-                board: board
+                board: board,
             }
         ));
     }
@@ -62,25 +64,30 @@ mod GameTest {
 
     #[test]
     fn test_tree() {
-        let mut root = Node::new(
+        let root = Node::new(
             vec![],
             None,
             Box::new(Board::new_base_game(2))
         );
 
-        let leaf = Node::new(
-            Vec::new(),
-            Some(root.clone()),
-            Box::new(root.borrow().board.draw_specific_card::<Neigh>()
-                 .unwrap().board)
-        );
+        {
+            let mut root_value = root.borrow_mut();
+    
+            let leaf = Node::new(
+                vec![],
+                Some(Rc::downgrade(&root)),
+                Box::new(root_value.board.draw_specific_card::<Neigh>()
+                    .unwrap().board)
+            );
+            root_value.children = vec![leaf.clone()];
+        }
 
-        root.borrow_mut().children = vec![leaf.clone()];
 
         let tree = GameTree {
             root: root
         };
 
         assert!(tree.root.borrow().children[0].borrow().board.deck.len() == 1);
+        assert!(Rc::ptr_eq(&tree.root.borrow().children[0].borrow().parent.as_ref().unwrap().upgrade().unwrap(), &tree.root));
     }
 }
